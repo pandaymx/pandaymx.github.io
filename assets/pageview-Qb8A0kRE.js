5c947aff47d8db72d8977d3d7d5698fd10feeb9f
pandaymx <82139672+pandaymx@users.noreply.github.com>
2023-12-12 09:27:52 +0000

    Deploying to gh-pages from @ pandaymx/ppmb@70375ea23e573f2836080218937de4037eaae49e 🚀

---
+++ assets/pageview-Qb8A0kRE.js
@@ -0,0 +1,7 @@
+import{a6 as t}from"./app-FtDMEW6a.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment.ppmb.xyz"};const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-FtDMEW6a.js").then(r=>r.a7),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
