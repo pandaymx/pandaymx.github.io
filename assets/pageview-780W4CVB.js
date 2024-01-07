9b482ea333570a7792c818dcaf9f2d6a7484f1c5
pandaymx <panda1943575780@outlook.com>
2024-01-07 16:18:38 +0000

    Deploying to gh-pages from @ pandaymx/ppmb@a91c021b58fe017a26ddb4d7f368ed0bc0292bcd 🚀

--- assets/pageview-780W4CVB.js
+++
@@ -1,7 +0,0 @@
-import{a6 as t}from"./app-620vAQYq.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment.ppmb.xyz"};const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-620vAQYq.js").then(r=>r.a7),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
