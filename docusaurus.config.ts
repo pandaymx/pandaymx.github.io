424a1b5a06f0514f59c0d84a3721f9a836502bc5
皮皮萌宝 <panda1943575780@outlook.com>
2025-08-15 21:17:27 +0800

    Update additional languages in Prism configuration from 'cs' to 'csharp'

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -157,7 +157,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
-      additionalLanguages: ['java','cs']
+      additionalLanguages: ['java','csharp']
     },
   } satisfies Preset.ThemeConfig,
 };
