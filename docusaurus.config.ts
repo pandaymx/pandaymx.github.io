3fbddaa97080b5fe04be28bca196f2f69ecd3f05
皮皮萌宝 <panda1943575780@outlook.com>
2025-12-18 23:14:28 +0800

    Add additional languages support for syntax highlighting in Docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -143,6 +143,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'bash'],
     },
   } satisfies Preset.ThemeConfig,
 };
