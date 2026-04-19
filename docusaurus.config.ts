213d848df07acc54abf3209a1a6da7836224cb8e
皮皮萌宝 <panda1943575780@outlook.com>
2026-04-19 12:52:02 +0800

    style: 更新 Prism 主题，亮色使用 oneLight，深色使用 oneDark

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -139,8 +139,8 @@ const config: Config = {
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
     prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.oneDark,
     },
   } satisfies Preset.ThemeConfig,
 };
